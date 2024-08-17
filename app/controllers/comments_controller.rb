93ef6d4c1723ff3cb40e835a416e9c423982e7a2
takamasa-tech <g1012075@icloud.com>
2024-08-17 11:14:15 +0900

    １５

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,22 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
-    if @comment.save
-      redirect_to prototype_path(@comment.prototype)
-    else
-      @prototype = @comment.prototype
-      @comments = @prototype.comments
-      flash.now[:alert] = 'コメントを入力してください。'
-      render "prototypes/show"
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to prototype_path(@comment.prototype) }
+        format.js
+      else
+        format.html {
+          @prototype = @comment.prototype
+          @comments = @prototype.comments
+          render "prototypes/show"
+        }
+        format.js { render :error }
+      end
     end
   end
 
-  private
-
-  def comment_params
+  private def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 end
